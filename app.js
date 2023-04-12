93aa76acbbc491a9dc1d97007bdb02a17ff70b0d
Autif Kamal <kautif@gmail.com>
2023-04-12 09:42:39 -0500

    initialized variables

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
+// Exercise 1 & Exercise 2
+const firstName = "Autif";
+let lastName = "Kamal";
+var age = 36;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
